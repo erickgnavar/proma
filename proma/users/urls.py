6192d532cdafa1976abb79915fbcf4a0714dfa5b
Erick Navarro <erick@navarro.io>
2018-03-22 18:06:53 -0700

    Add change password page

--- proma/users/urls.py
+++ proma/users/urls.py
@@ -7,4 +7,9 @@ app_name = 'users'
 urlpatterns = [
     path('login', auth_views.LoginView.as_view(template_name='login.html', success_url='/'), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path(
+        'change-password',
+        auth_views.PasswordChangeView.as_view(template_name='users/change_password.html', success_url='/'),
+        name='change-password',
+    ),
 ]
