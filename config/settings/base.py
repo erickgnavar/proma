d60fa736d644f09aa61383ab0818a485533b9815
Erick Navarro <erick@navarro.io>
2018-12-21 10:02:03 -0500

    Add dj-database-url for easiest database config

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+import dj_database_url
 from django.contrib.messages import constants as messages
 
 
@@ -91,17 +92,8 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
-        "PORT": os.environ.get("DB_PORT", 5432),
-        "ATOMIC_REQUESTS": True,
-    }
-}
+DATABASES = {"default": dj_database_url.config()}
+DATABASES["default"].update({"ATOMIC_REQUESTS": True})
 
 
 # Password validation
