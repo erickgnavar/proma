e254acb66a63d9ffa06c156a6c1dd8cc89457530
Erick Navarro <erick@navarro.io>
2020-09-26 15:24:28 -0500

    Add setting to handle email TLS

--- config/settings/base.py
+++ config/settings/base.py
@@ -146,6 +146,7 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "0") == "1"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
